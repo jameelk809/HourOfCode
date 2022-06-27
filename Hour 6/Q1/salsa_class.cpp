1110b59bb86a2bb98a3cd293b25fd8fd1a2ac6b4
Kamran <jameelk809@gmail.com>
2022-06-27 19:42:19 +0530

    salsa_class.cpp done

--- Hour 6/Q1/salsa_class.cpp
+++ Hour 6/Q1/salsa_class.cpp
@@ -2,8 +2,11 @@
 using namespace std;
 
 int main(){
-    string queue_A = "00111101";
-    string queue_B = "10000001";
+    // string queue_A = "00111101";
+    // string queue_B = "10000001";
+    string queue_A, queue_B;
+    cin>>queue_A;
+    cin>>queue_B;
     int len = queue_A.length(), i, count = 0;
     for(i = 0; i < len; i++){
         if(queue_A[i] != queue_B[i])
