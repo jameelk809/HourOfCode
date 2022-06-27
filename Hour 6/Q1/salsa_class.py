c708b46c7d6a979d458418571c4a931b2aa80cf6
Kamran <jameelk809@gmail.com>
2022-06-27 19:23:24 +0530

    salsa_class.py done

--- Hour 6/Q1/salsa_class.py
+++ Hour 6/Q1/salsa_class.py
@@ -1,5 +1,7 @@
-queue_A = '00110100'
-queue_B = '00000111'
+# queue_A = '00110100'
+# queue_B = '00000111'
+queue_A = input()
+queue_B = input()
 count = 0
 for i in range(len(queue_A)):
     if queue_A[i] != queue_B[i]:
