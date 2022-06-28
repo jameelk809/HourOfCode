f0171fccccb8c9ca4a856b387d1ac83293af9ff4
Kamran <jameelk809@gmail.com>
2022-06-28 23:13:17 +0530

    Q1 in py done

---
+++ Hour 8/Q1/adjacent_XOR.py
@@ -0,0 +1,16 @@
+n = int(input())
+binary = ''
+while n > 0:
+    r = n % 2
+    n //= 2
+    binary = str(r) + binary
+    # this binary is stored in reverse (correct) order.
+
+XOR_result = ''
+for i in range(0, len(binary)-1):
+    XOR_result += str(int(binary[i]) ^ int(binary[i + 1]))
+XOR_result += str(int(binary[-1]) ^ 1)
+res = 0
+for i in range(len(XOR_result)):
+    res += int(XOR_result[i]) * 2 ** (len(XOR_result) - 1 - i)
+print(res)
