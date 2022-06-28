cb34fbd6d252429d6db713a4a027c02339bfe46f
Kamran <jameelk809@gmail.com>
2022-06-28 23:15:46 +0530

    Update adjacent_XOR.py

--- Hour 8/Q1/adjacent_XOR.py
+++ Hour 8/Q1/adjacent_XOR.py
@@ -3,13 +3,11 @@ binary = ''
 while n > 0:
     r = n % 2
     n //= 2
-    binary = str(r) + binary
-    # this binary is stored in reverse (correct) order.
-
+    binary = str(r) + binary                                # this binary is stored in reverse (correct) order.
 XOR_result = ''
 for i in range(0, len(binary)-1):
     XOR_result += str(int(binary[i]) ^ int(binary[i + 1]))
-XOR_result += str(int(binary[-1]) ^ 1)
+XOR_result += str(int(binary[-1]) ^ 1)                      # last bit is XORed with 1
 res = 0
 for i in range(len(XOR_result)):
     res += int(XOR_result[i]) * 2 ** (len(XOR_result) - 1 - i)
