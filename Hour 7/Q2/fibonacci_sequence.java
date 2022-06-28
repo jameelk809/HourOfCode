2b97581b4f401d2f42b9c43637af6ee02fd523a3
Kamran <jameelk809@gmail.com>
2022-06-28 17:06:14 +0530

    Q2 in java done

--- Hour 7/Q2/fibonacci_sequence.java
+++ Hour 7/Q2/fibonacci_sequence.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class fibonacci_sequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+            int n = sc.nextInt(), i, count = 0;
+            int arr[] = new int[n];
+            for(i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+            for (int x : arr) {
+                if(is_Fibonacci(x)==true)
+                    count++;
+            }
+            System.out.print(count);
+        }
+        finally{
+            sc.close();
+        }
+    }
     
+    public static boolean is_Perfect_Square(int K) {
+        int s = (int)Math.sqrt(K);
+        return ((s*s == K) ? true: false);
+    }
+
+    public static boolean is_Fibonacci(int R){
+        return(is_Perfect_Square(5*R*R + 4) || is_Perfect_Square(5*R*R - 4));
+    }
 }
